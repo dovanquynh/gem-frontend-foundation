e4c824fd265c33826c8829a0853520ef1ff84cfa
Administrator <administrator@Administrators-iMac-133.local>
2018-06-06 18:00:04 +0700

    sua lan 3

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function () {
+    var btnNav = document.querySelector('.navbar-custom-icon');
+    var sideBar = document.querySelector('.sidebar');
+    var main = document.querySelector('#main');
+
+},false);
